5f89d43f501c45b724fb3d863a68c9d2e4dc9740
iDhanush <idhanushm@gmail.com>
2024-11-02 00:43:02 +0530

    ai utils

--- ai_func/utils.py
+++ ai_func/utils.py
@@ -1,3 +1,14 @@
+from global_vars import Var
 import google.generativeai as genai
+from google.generativeai import GenerativeModel
 
-genai.configure(api_key='')
+genai.configure(api_key=Var.gemini_api_key)
+model = GenerativeModel("gemini-1.5-flash")
+
+
+def generate_ai_output(prompt):
+    try:
+        res = model.generate_content(prompt)
+        return res.text
+    except Exception as e:
+        print("AI ERROR", e)
