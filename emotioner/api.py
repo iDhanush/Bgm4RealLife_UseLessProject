c45fe1889dbc6a7537c9085f81ead75859a3745d
iDhanush <idhanushm@gmail.com>
2024-11-01 23:59:38 +0530

    payload reciving interfacee

--- emotioner/api.py
+++ emotioner/api.py
@@ -4,4 +4,12 @@ from pydantic import BaseModel, constr
 emo_router = APIRouter(tags=['emotioner'])
 
 
+class EmotionerData(BaseModel):
+    spoken_text: str
+    lang: constr(pattern='^(mal|eng)$')
 
+
+@emo_router.get('/get_emotion')
+def emotioner(emotioner_data: EmotionerData):
+    spoken_text = emotioner_data.spoken_text
+    lang = emotioner_data.lang
