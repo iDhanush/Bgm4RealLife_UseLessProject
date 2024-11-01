dac7bbcdf3d2c21faff3ab42b13dabcf2130bd5c
iDhanush <idhanushm@gmail.com>
2024-11-02 04:12:50 +0530

    prompt change

--- emotioner/utils.py
+++ emotioner/utils.py
@@ -1,19 +1,23 @@
 def parse_prompt(spoken_txt, lang):
     lang = 'Malayalam' if lang == 'mal' else 'English'
-    return f'''"{spoken_txt}"
+    prompt = f'''"{spoken_txt}"
 What's is the emotion u see in the above phrase written in {lang} From
-Happy
-Angry
-Surprise 
-Disgusting 
-Depressed and sad
-In love
-Excited
+happy
+angry
+disgusting 
+depressed and sad
+in love
+exciting
 superb and existing entry
 
 If the person is not talking about himself then predict the emotion of the person he's talking to as $emotion_response (depressed if angry) from the list of emotions 
 Only send the emotion and also include if the person is talking about someone else or himself u select without any sentence 
-''' + '''{emotion: $emotion 
-emotional response: $emotional_response
-Talking about: $himself | $someone
+''' + '''
+
+SEND AS DICT WITHOUT ANY OTHER TEXT without any markup
+output_structure (all lowercase & only json no other text)
+RESULT: {"emotion": "$emotion" 
+"emotional response": "$emotional_response"
+"talking about": "$himself | $someone"
 }'''
+    return prompt
