37f90c14ef500dbb98fb6a29f4755b0945c16a8f
iDhanush <idhanushm@gmail.com>
2024-12-28 00:29:24 +0530

    support multiple api keys

--- global_vars.py
+++ global_vars.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 class Var:
     base_url = os.environ.get("BASE_URL", "http://localhost:8000")
-    gemini_api_key = os.environ.get("GEMINI_API_KEY")
+    gemini_api_keys = os.environ.get("GEMINI_API_KEY").split(' ')
